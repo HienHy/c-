7908bdbd40a70951ee5ec2e9f1c3f28dc0eebab7
hiennd <hiennguyenhy0911@gmail.com>
2023-01-05 08:18:21 +0700

    a

---
+++ T2204M_C/ss1/Fraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace T2204M_C.ss1
+{
+	public class Fraction
+
+	{
+		int tuSo;
+		int mauSo;
+
+		public Fraction(){}
+
+        public Fraction(int tuSo, int mauSo)
+        {
+            this.TuSo = tuSo;
+            this.MauSo = mauSo;
+        }
+
+        public int TuSo { get => tuSo; set => tuSo = value; }
+        public int MauSo { get => mauSo; set => mauSo = value; }
+        public void printPhanSo()
+        {
+            Console.WriteLine("Phan so = " + tuSo + "/" +mauSo );
+
+        }
+
+       
+    }
+}
+
---
+++ T2204M_C/ss1/Test.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+namespace T2204M_C.ss1
+{
+	public class Test
+	{
+        public static void Main(string[] args)
+        {
+            Fraction fraction = new Fraction(1,2);
+            fraction.printPhanSo();
+
+        }
+
+
+    }
+}
+
