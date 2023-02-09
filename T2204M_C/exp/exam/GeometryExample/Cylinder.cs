cb39c6fc22202bfcf5f708c8b2ac38e63c14a556
hienhy <101379678+HienHy@users.noreply.github.com>
2023-02-09 10:26:41 +0700

    cm

---
+++ T2204M_C/exp/exam/GeometryExample/Cylinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace T2204M_C.exp.exam.GeometryExample
+{
+	public class Cylinder
+	{
+		public Cylinder()
+		{
+		}
+	}
+}
+
---
+++ T2204M_C/exp/exam/exercise2/AbstracAnimal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace T2204M_C.exp.exam.exercise2
+{
+	public class AbstracAnimal
+	{
+		public AbstracAnimal()
+		{
+		}
+	}
+}
+
